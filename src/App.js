511acbb0db01fc3fbcef3b62d2ac9a54f6523af6
doanphiho <doanphj5@gmail.com>
2021-06-07 10:07:15 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,90 +1,104 @@
-import './App.css';
-import { useState } from 'react';
-import { chia, cong, heSo, nhan, sobu2, tru } from './func/funAll';
+import "./App.css";
+import { useState } from "react";
+import { chia, cong, heSo, nhan, sobu2, tru } from "./func/funAll";
 
 function App() {
-  const [a, setA] = useState('');
-  const [b, setB] = useState('');
-  const [c, setC] = useState(heSo.he10)
-  const [d, setD] = useState()
-  if (d) console.log(d)
+  const [a, setA] = useState("");
+  const [b, setB] = useState("");
+  const [c, setC] = useState(heSo.he10);
+  const [d, setD] = useState();
+  if (d) console.log(d);
   return (
     <div className="App">
-      <input value={a} onChange={(e) => setA(c => c = e.target.value)} />
-      <input value={b} onChange={(e) => setB(c => c = e.target.value)} />
-      <button onClick={() => setC(c => c = c === heSo.he10 ? heSo.he2 : heSo.he10)}>{c}</button>
-      <button onClick={() => setD(g => g = cong({ a: a, b: b, c: c }))} >Cong</button>
-      <button onClick={() => setD(g => g = tru(a, b, c))} >Tru</button>
-      <button onClick={() => setD(g => g = nhan(a, b, c))} >Nhan</button>
-      <button onClick={() => setD(g => g = chia(a, b, c))} >Chia</button>
+      <input value={a} onChange={(e) => setA((c) => (c = e.target.value))} />
+      <input value={b} onChange={(e) => setB((c) => (c = e.target.value))} />
+      <button
+        onClick={() =>
+          setC((c) => (c = c === heSo.he10 ? heSo.he2 : heSo.he10))
+        }
+      >
+        {c}
+      </button>
+      <button onClick={() => setD((g) => (g = cong({ a, b, c })))}>
+        Cong
+      </button>
+      <button onClick={() => setD((g) => (g = tru({ a, b, c })))}>Tru</button>
+      <button onClick={() => setD((g) => (g = nhan({ a, b, c })))}>Nhan</button>
+      <button onClick={() => setD((g) => (g = chia(a, b, c)))}>Chia</button>
       <center>
         <table>
           <thead>
-            {
-              d && (d.t === '+' || d.t === '-') && <tr key={`index`}>
+            {d && (d.t === "+" || d.t === "-") && (
+              <tr key={`index`}>
                 <th>A + B</th>
                 <th>bit mượn</th>
               </tr>
-            }
-            {
-              d && d.t === '*' && <tr key={`index`}>
+            )}
+            {d && d.t === "*" && (
+              <tr key={`index`}>
                 <th>Bước thực hiện</th>
                 <th>Giá trị</th>
               </tr>
-            }
-            {
-              d && d.t === ':' && <tr key={`index`}>
+            )}
+            {d && d.t === ":" && (
+              <tr key={`index`}>
                 <th>Bước thực hiện</th>
                 <th>Số dư</th>
                 <th>Giá trị</th>
               </tr>
-            }
+            )}
           </thead>
           <tbody>
-            {
-              d && (d.t === '+' || d.t === '-') && d.g.map((item, i) => <tr key={`+${i}`}>
-                <td>{item.d}</td>
-                <td>{item.ischeck}</td>
-              </tr>)
-            }
-            {
-              d && (d.t === '+' || d.t === '-') && <tr key={`+${d.d}`}>
+            {d &&
+              (d.t === "+" || d.t === "-") &&
+              d.g.map((item, i) => (
+                <tr key={`+${i}`}>
+                  <td>{item.d}</td>
+                  <td>{item.ischeck}</td>
+                </tr>
+              ))}
+            {d && (d.t === "+" || d.t === "-") && (
+              <tr key={`+${d.d}`}>
                 <td>Đáp án:</td>
                 <td>{d.d}</td>
               </tr>
-            }
-            {
-              d && d.t === '-' && <tr key={`--${d.d}`}>
+            )}
+            {d && d.t === "-" && (
+              <tr key={`--${d.d}`}>
                 <td>Số bù 2:</td>
                 <td>{sobu2(d.d)}</td>
               </tr>
-            }
-            {
-              d && d.t === '*' && d.g.map((item, i) => <tr key={`+${i}`}>
-                <td>Lần {i + 1}</td>
-                <td>{item}</td>
-              </tr>)
-            }
-            {
-              d && d.t === '*' && <tr key={`+${d.d}`}>
+            )}
+            {d &&
+              d.t === "*" &&
+              d.g.map((item, i) => (
+                <tr key={`+${i}`}>
+                  <td>Lần {i + 1}</td>
+                  <td>{item}</td>
+                </tr>
+              ))}
+            {d && d.t === "*" && (
+              <tr key={`+${d.d}`}>
                 <td>Đáp án:</td>
                 <td>{d.d}</td>
               </tr>
-            }
-            {
-              d && d.t === ':' && d.g.map((item, i) => <tr key={`+${i}`}>
-                <td>Lần {i + 1}</td>
-                <td>{item.g}</td>
-                <td>{item.k}</td>
-              </tr>)
-            }
-            {
-              d && d.t === ':' && <tr key={`+${d.d}`}>
+            )}
+            {d &&
+              d.t === ":" &&
+              d.g.map((item, i) => (
+                <tr key={`+${i}`}>
+                  <td>Lần {i + 1}</td>
+                  <td>{item.g}</td>
+                  <td>{item.k}</td>
+                </tr>
+              ))}
+            {d && d.t === ":" && (
+              <tr key={`+${d.d}`}>
                 <td>Đáp án:</td>
                 <td>{d.l}</td>
                 <td>{d.d}</td>
               </tr>
-            }
+            )}
           </tbody>
         </table>
       </center>
--- src/func/funAll.js
+++ src/func/funAll.js
@@ -1,94 +1,114 @@
-const sobu1 = (a) => a.split('').map((i) => i === '0' ? '1' : '0').join('')
-const sobu2 = (a) => cong('1', Number(a), toString(), true).d
-const tru = (a, b, c) => {
-    if (!c) {
-        a = Number(a).toString(2)
-        b = Number(b).toString(2)
+const sobu1 = (a) =>
+  a
+    .split("")
+    .map((i) => (i === "0" ? "1" : "0"))
+    .join("");
+const sobu2 = (a) => cong({ a: "1", b: a, c: heSo.he2 }).d;
+const nhan = ({ a = "", b = "", c = heSo.he10 }) => {
+  let a10 = c !== heSo.he10 ? parseInt(a, 2) : a;
+  let b10 = c !== heSo.he10 ? parseInt(b, 2) : b;
+  let a2 = c === heSo.he10 ? Number(a).toString(2) : a;
+  let b2 = c === heSo.he10 ? Number(b).toString(2) : b;
+  const cList = b2
+    .split("")
+    .map((c, i) =>
+      c === "1" ? `${a2}${"0".repeat(i)}` : `${"0".repeat(i + a2.length)}`
+    );
+  let d = "0";
+  for (let i = 0; i < cList.length; i++) {
+    const { d: df } = cong({ a: d, b: cList[i], c: heSo.he2 });
+    d = df;
+  }
+  return { a10, b10, a2, b2, d, g: cList, t: "*" };
+};
+const chia = ({ a = "", b = "", c = heSo.he10 }) => {
+  let a10 = c !== heSo.he10 ? parseInt(a, 2) : a;
+  let b10 = c !== heSo.he10 ? parseInt(b, 2) : b;
+  let a2 = c === heSo.he10 ? Number(a).toString(2) : a;
+  let b2 = c === heSo.he10 ? Number(b).toString(2) : b;
+  const aList = a2.split("");
+  let g = "0";
+  let i = 0;
+  let k = "";
+  let list = [];
+  while (i < a2.length) {
+    g = g + aList[i];
+    i++;
+    if (parseInt(g, 2) < parseInt(b, 2)) {
+      k = k + "0";
+    } else {
+      const ls = tru(g, b, true);
+      g = sobu2(ls.d);
+      k = k + "1";
     }
-    b = '0'.repeat(((parseInt(b.length / 8) + 1) * 8) - b.length) + b
-    b = sobu1(b)
-    const _cong = cong(a, b, true)
-    return { ..._cong, t: '-' }
-}
-const nhan = (a, b, c) => {
-    if (!c) {
-        a = Number(a).toString(2)
-        b = Number(b).toString(2)
-    }
-    const cList = b.split('').map((c, i) => c === '1' ? `${a}${'0'.repeat(i)}` : `${'0'.repeat(i + a.length)}`)
-    let d = '0'
-    for (let i = 0; i < cList.length; i++) {
-        const ls = cong(d, cList[i], true)
-        d = ls.d
-    }
-    return { d, g: cList, t: '*' }
-}
-const chia = (a, b, c) => {
-    if (!c) {
-        a = Number(a).toString(2)
-        b = Number(b).toString(2)
-    }
-    const aList = a.split('')
-    let g = '0'
-    let i = 0;
-    let k = ''
-    let list = []
-    while (i < a.length) {
-        g = g + aList[i]
-        i++
-        if (parseInt(g, 2) < parseInt(b, 2)) {
-            k = k + '0'
-        }
-        else {
-            const ls = tru(g, b, true)
-            g = sobu2(ls.d)
-            k = k + '1'
-        }
-        list.push({ g, i, k })
-    }
-    return { d: k, g: list, t: ':', l: g }
-}
+    list.push({ g, i, k });
+  }
+  return { d: k, g: list, t: ":", l: g };
+};
 
 const heSo = {
-    he10: 'Hệ số 10',
-    he2: 'Hệ số 2'
-}
+  he10: "Hệ số 10",
+  he2: "Hệ số 2",
+};
 
 const phepCong = {
-    a: '00',
-    b: '01',
-    c: '10',
-    d: '11',
-}
+  a: "00",
+  b: "01",
+  c: "10",
+  d: "11",
+};
 
-const s = ({ a = '0', b = '0', c = true }) => [a, b].join('') === phepCong.a ?
-    { e: c, f: false } : [a, b].join('') === phepCong.b
-        || [a, b].join('') === phepCong.c ? { e: !c, f: c } : { e: c, f: !c }
+const s = ({ a = "0", b = "0", c = true }) =>
+  [a, b].join("") === phepCong.a
+    ? { e: c, f: false }
+    : [a, b].join("") === phepCong.b || [a, b].join("") === phepCong.c
+    ? { e: !c, f: c }
+    : { e: c, f: true };
 
-const formatBin = ({ a = '0', b = '0' }) => {
-    const i = [2, 3, 4, 5, 6, 7].filter(c => c ** 2 >= a.length && c ** 2 >= b.length)[0]
-    return { a: '0'.repeat(i ** 2 - a.length) + a, b: '0'.repeat(i ** 2 - b.length) + b }
-}
+const formatBin = ({ a = "0", b = "0" }) => {
+  const i = [2, 3, 4, 5, 6, 7].filter(
+    (c) => c ** 2 >= a.length && c ** 2 >= b.length
+  )[0];
+  return {
+    a: "0".repeat(i ** 2 - a.length) + a,
+    b: "0".repeat(i ** 2 - b.length) + b,
+  };
+};
 
+const cong = ({ a = "", b = "", c = heSo.he10 }) => {
+  let a10 = c !== heSo.he10 ? parseInt(a, 2) : a;
+  let b10 = c !== heSo.he10 ? parseInt(b, 2) : b;
+  let a2 = c === heSo.he10 ? Number(a).toString(2) : a;
+  let b2 = c === heSo.he10 ? Number(b).toString(2) : b;
+  let k = false;
+  const { a: af, b: bf } = formatBin({ a: a2, b: b2 });
+  const d = af
+    .split("")
+    .reverse()
+    .map((it, ix) => {
+      const { e, f } = s({
+        a: it,
+        b: bf.split("").reverse()[ix],
+        c: k,
+      });
+      k = f;
+      return e ? "1" : "0";
+    })
+    .reverse()
+    .join("");
+  console.log((k ? "1" : "0") + d, parseInt((k ? "1" : "0") + d, 2));
+  return { a10, b10, a2, b2, k: k ? "1" : "0", d };
+};
 
-const cong = ({ a = '', b = '', c = heSo.he10 }) => {
-    let a10 = (c !== heSo.he10 ? parseInt(a, 2) : a)
-    let b10 = (c !== heSo.he10 ? parseInt(b, 2) : b)
-    let a2 = (c === heSo.he10 ? Number(a).toString(2) : a)
-    let b2 = (c === heSo.he10 ? Number(b).toString(2) : b)
-    let k = false
-    const { a: af, b: bf } = formatBin({ a: a2, b: b2 })
-    const d = af.split('').reverse().map((it, ix) => {
-        const l = s({
-            a: it,
-            b: bf.split('').reverse()[ix],
-            c: k
-        })
-        k = l.f
-        return l.e ? '1' : '0'
-    }).reverse().join('')
-    console.log((k ? '1' : '0') + d, parseInt((k ? '1' : '0') + d, 2));
-    return { a10, b10, a2, b2, k: k ? '1' : '0', d }
-}
+const tru = ({ a = "", b = "", c = heSo.he10 }) => {
+  let a10 = c !== heSo.he10 ? parseInt(a, 2) : a;
+  let b10 = c !== heSo.he10 ? parseInt(b, 2) : b;
+  let a2 = c === heSo.he10 ? Number(a).toString(2) : a;
+  let b2 = c === heSo.he10 ? Number(b).toString(2) : b;
+  const { a: af, b: bf } = formatBin({ a: a2, b: b2 });
+  b = sobu2(sobu1(bf));
+  const { d, k } = cong({ a: af, b: b, c: heSo.he2 });
+  return { a10, b10, a2, b2, k, d, bbu1: b };
+};
 
-export { cong, tru, sobu2, nhan, chia, heSo }
+export { cong, tru, sobu2, nhan, chia, heSo };
